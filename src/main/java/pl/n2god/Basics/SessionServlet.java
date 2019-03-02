a11b97e50d8434342a371a50fc18fc127b059a4c
[n2god] <[n2god@o2.pl]>
2019-03-02 13:03:20 +0100

    rename packages propertly

---
+++ src/main/java/pl/n2god/Basics/SessionServlet.java
@@ -0,0 +1,26 @@
+package pl.n2god.Basics;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class SessionServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+
+        PrintWriter writer = resp.getWriter();
+        //sprawdzamy id sesji
+        writer.println(session.getId());
+
+        //czas utworzenia
+        writer.println(session.getCreationTime());
+
+
+    }
+}
--- src/main/java/pl/n2god/redirect/FirstRedirectServlet.java
+++ src/main/java/pl/n2god/redirect/FirstRedirectServlet.java
@@ -6,12 +6,25 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 
 @WebServlet (name = "FirstRedirectServlet", urlPatterns = "/redirect")
 public class FirstRedirectServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+        writer.println("Servlet 1: before redirect");
+        //wykonujemy przekierowanie zwykłe, lecą dwa zapytania po stronie serwera
+        //resp.sendRedirect("/redirect-target");
 
+        //wykonujemy przekierowanie za pomocą forward - czyli 1: pierwszy servlet, następnie redirect do drugiego i odpowiada responsem drugi servlet, a nie ten pierwszy
+        //leci tylko jedno zapytanie po stronie serwera
+        //req.getRequestDispatcher("/redirect-target").forward(req, resp); //tu mozna zamiast forward uzyc include
+
+        //przekierowanie include - czeka na odpowiedz, serwlety gadaja pomiedzy soba, ale odpowiada ten pierwszy
+        req.getRequestDispatcher("redirect-target").include(req, resp);
+
+        writer.println("Servlet 1: after redirect");
     }
 }
